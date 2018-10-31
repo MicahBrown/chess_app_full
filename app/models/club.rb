b03eb9277d945d56b9e75b39c3539916461e6546
Micah Brown <micahyb@gmail.com>
2018-10-30 18:14:53 -0600

    generate UIDs for clubs on creation

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,13 @@ class Club < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :club_memberships
   has_many :users, through: :club_memberships
+
+  before_create :set_uid
+
+  def set_uid
+    self.uid = loop do
+      token =  SecureRandom.base58(5)
+      break token unless Club.where("LOWER(uid) = ?", token.downcase).exists?
+    end
+  end
 end
