eee5b40a8eb13834170ba1a86963134c9c02fbb2
Micah Brown <micahyb@gmail.com>
2019-01-06 15:17:49 -0700

    added new game creation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+  def index
+  end
+
+  def create
+    game = Game.new
+
+    if game.save
+      redirect_to game_path(game), notice: "Successfully started new game!"
+    else
+      redirect_to games_path, alert: "Unable to start new game."
+    end
+  end
+
+  def show
+  end
 end
