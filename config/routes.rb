d877f992f6b240a175e2c37a2a11c25fbd58bacf
Micah Brown <micahyb@gmail.com>
2019-01-06 11:07:00 -0700

    added page for play tab

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :clubs
   resources :users, only: [:show]
+  resources :games, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
