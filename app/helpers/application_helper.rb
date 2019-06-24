8dcbe973c808e19072fa59b1c77fda013dd8465d
Micah Brown <micahyb@gmail.com>
2019-06-23 20:52:45 -0600

    include type in data attribute

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def piece(name, white=false)
     color = white ? 'white' : 'black'
-    content_tag :span, image_tag("#{color}_#{name}.svg"), class: "piece", data: {color: color}
+    content_tag :span, image_tag("#{color}_#{name}.svg"), class: "piece", data: {color: color, type: name}
   end
 end
