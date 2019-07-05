2a2a4a96a5a0ed51c3fa0f6a14a51209ed2411d7
Micah Brown <micahyb@gmail.com>
2019-07-04 21:42:42 -0600

    added link for piece to move

--- db/migrate/20190702043048_create_game_moves.rb
+++ db/migrate/20190702043048_create_game_moves.rb
@@ -2,6 +2,7 @@ class CreateGameMoves < ActiveRecord::Migration[5.2]
   def change
     create_table :game_moves do |t|
       t.belongs_to :game, null: false, foreign_key: true
+      t.belongs_to :piece, null: false, foreign_key: true
       t.integer :team, null: false
       t.timestamps null: false
     end
